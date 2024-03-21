6762ec5bc6cbb6851daae5a1c8beab9516000dff
s87771 <s87771@bht-berlin.de>
2024-03-21 03:32:21 +0100

    conenction to database mariaDB, sign up and login with password hashing

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,105 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\Security\Core\Exception\BadCredentialsException;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+
+class BlogController extends AbstractController
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/')]
+    public function homePAge(Request $request): Response
+    {
+        return $this->render('base.html.twig');
+    }
+
+    #[Route('/signup', name: 'user-signup', methods: ['GET'])]
+    public function showPage(Request $request): Response
+    {
+        return $this->render('User/signup.html.twig');
+    }
+
+    #[Route('/signup', name: 'signup', methods: ['POST'])]
+    public function saveData(Request $request, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $username = $request->request->get('username');
+        $password = $request->request->get('password');
+
+        $user = new User();
+
+        $user->setUsername($username);
+
+        $hashedPassword = $passwordHasher->hashPassword($user, $password);
+
+        $user->setPassword($hashedPassword);
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        // $users = $this->entityManager->getRepository(User::class)->findAll();
+
+        return $this->redirectToRoute('login_page');
+    }
+
+
+    #[Route('/login', name: 'login_page', methods: ['GET'])]
+    public function loginPage(Request $request): Response
+    {
+        return $this->render('User/login.html.twig');
+    }
+
+    #[Route('/login', name: 'login', methods: ['POST'])]
+    public function login(Request $request, UserPasswordHasherInterface $passwordHasher, SessionInterface $session): Response
+    {
+        $username = $request->request->get('username');
+        $password = $request->request->get('password');
+
+        $user = $this->entityManager->getRepository(User::class)->findOneBy(['username' => $username]);
+
+        if (!$user) {
+            throw new BadCredentialsException('Bad Credentials');
+        }
+
+        if (!$passwordHasher->isPasswordValid($user, $password)) {
+            throw new BadCredentialsException('Bad Credentials');
+        } else {
+            $session->set('username', $username);
+            return $this->redirectToRoute('personalpage');
+        }
+    }
+
+
+    #[Route('/personalpage', name: 'personalpage', methods: ['GET'])]
+    public function personalpage(Request $request, SessionInterface $session): Response
+    {
+        $username = $session->get('username');
+        if (!$username) {
+            return $this->redirectToRoute('login');
+        }
+        return $this->render(
+            'Logged_in/personalpage.html.twig',
+            ['username' => $username]
+        );
+    }
+}
---
+++ src/Entity/Blog.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BlogRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BlogRepository::class)]
+class Blog
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $username = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $body = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $picture = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): static
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getBody(): ?string
+    {
+        return $this->body;
+    }
+
+    public function setBody(?string $body): static
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    public function getPicture(): ?string
+    {
+        return $this->picture;
+    }
+
+    public function setPicture(?string $picture): static
+    {
+        $this->picture = $picture;
+
+        return $this;
+    }
+}
